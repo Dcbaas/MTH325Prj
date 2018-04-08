49b6cac53388b718fef379c87df12a54a11ea35f
Dcbaas <Dcbaas@github.com>
2018-04-08 17:48:14 -0400

    Wrote power function

--- Bipartie&Matchings.py
+++ Bipartie&Matchings.py
@@ -1,2 +1,11 @@
 def power(list):
-    
+    output_list = []
+    pow_set_size = pow(2, len(list))
+    print("here 1")
+    for i in range(0, pow_set_size):
+        temp = []
+        for j in range(0, len(list)):
+            if i & (1 << j):
+                temp.append(list[j])
+        output_list.append(temp)
+    return output_list
